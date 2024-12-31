677f1349a7b3d457f2b2ea2b22e28a28e78a8974
luq-mne <luqman.yazrie@outlook.com>
2025-01-01 07:38:08 +0800

    Added colors to frame

--- src/components/Frames.jsx
+++ src/components/Frames.jsx
@@ -1,7 +1,10 @@
 export const Frame = (props) => {
+    const baseColor = props.light ? 'bg-[rgb(225,225,225)]' : 'bg-[rgb(0,0,0)]';
+    const outlineColor = props.light ? 'bg-[rgb(205,205,205)]' : 'bg-[rgb(20,20,20)]';
+
     return (
-        <div class="p-2 rounded-md bg-[rgb(20,20,20)]">
-            <div class="p-2 rounded-md bg-black">
+        <div class={`p-2 rounded-md ${outlineColor}`}>
+            <div class={`p-2 rounded-md ${baseColor}`}>
                 {props.children}
             </div>
         </div>
@@ -9,13 +12,17 @@ export const Frame = (props) => {
 }
 
 export const FrameWithHeading = (props) => {
+    const baseColor = props.light ? 'bg-[rgb(225,225,225)]' : 'bg-[rgb(0,0,0)]';
+    const outlineColor = props.light ? 'bg-[rgb(205,205,205)]' : 'bg-[rgb(20,20,20)]';
+    const headingColor = props.light ? 'text-[rgb(0,0,0)]' : 'text-[rgb(225,225,225)]';
+
     return (
-        <div class="p-2 flex flex-col gap-2 rounded-md bg-[rgb(20,20,20)]">
-            <h1 class="text-lg text-[rgb(225,225,225)] font-mono font-semibold">
+        <div class={`p-2 flex flex-col gap-2 rounded-md ${outlineColor}`}>
+            <h1 class={`text-lg ${headingColor} font-mono font-semibold`}>
                 {props.heading}
             </h1>
 
-            <div class="p-2 rounded-md bg-black">
+            <div class={`p-2 rounded-md ${baseColor}`}>
                 {props.children}
             </div>
         </div>
