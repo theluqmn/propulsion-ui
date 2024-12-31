78ca8b9b77710266cb661fdf89f9952ddd193891
luq-mne <luqman.yazrie@outlook.com>
2024-12-31 23:25:48 +0800

    worked on a header or smth

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,48 @@ import { Button, ButtonWithInlineText } from "./components/Buttons";
 
 export default function App() {
     return (
-        <div class="p-4 flex flex-col gap-4">
-            <TextWithHeading heading="Testing" text="Hello World" />
-            <Text text="This is an epic text" />
-            <TextLink href="https://google.com" text="Google" />
+        <div>
+            <header class="p-4 flex flex-row justify-between items-center bg-[rgb(30,30,30)]">
+                <h1 class="text-2xl font-semibold font-mono text-[rgb(225,225,225)]">
+                    Propulsion UI
+                </h1>
 
-            <Button text="Click Me" light="true" />
-            <Button text="Click Me" />
-            <ButtonWithInlineText buttonText="Select" text="Click Me" />
+                <Frame>
+                    <div class="flex flex-row gap-4">
+                        <a href="https://github.com/Propulsion-UI/Propulsion-UI" class="text-white">
+                            GitHub
+                        </a>
 
-            <Frame>
-                <div class="grid grid-cols-2 gap-4">
-                    <Button text="Click Me" light="true" />
-                    <Button text="Click Me" light="true" />
-                </div>
-            </Frame>
+                        <a href="https://discord.gg/propulsionui" class="text-white">
+                            Discord
+                        </a>
+                    </div>
+                </Frame>
+            </header>
 
-            <FrameWithHeading heading="This is a heading for a frame">
-                <div class="grid grid-cols-2 gap-4">
-                    <Button text="Click Me" light="true" />
-                    <Button text="Click Me" light="true" />
-                </div>
-            </FrameWithHeading>
+            <div class="p-4 flex flex-col gap-4">
+                <TextWithHeading heading="Testing" text="Hello World" />
+                <Text text="This is an epic text" />
+                <TextLink href="https://google.com" text="Google" />
+
+                <Button text="Click Me" light="true" />
+                <Button text="Click Me" />
+                <ButtonWithInlineText buttonText="Select" text="Click Me" />
+
+                <Frame>
+                    <div class="grid grid-cols-2 gap-4">
+                        <Button text="Click Me" light="true" />
+                        <Button text="Click Me" light="true" />
+                    </div>
+                </Frame>
+
+                <FrameWithHeading heading="This is a heading for a frame">
+                    <div class="grid grid-cols-2 gap-4">
+                        <Button text="Click Me" light="true" />
+                        <Button text="Click Me" light="true" />
+                    </div>
+                </FrameWithHeading>
+            </div>
         </div>
     );
 }
