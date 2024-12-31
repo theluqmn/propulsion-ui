b69a05ccdb471f610cb5cafed1a67dca530c1845
luq-mne <luqman.yazrie@outlook.com>
2025-01-01 07:25:27 +0800

    light theme on buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -21,7 +21,7 @@ export const ButtonWithInlineText = (props) => {
     return (
         <div class="flex flex-row gap-2 items-center">
             <div class="p-2 inline-block rounded-md bg-[rgb(20,20,20)]">
-                <button class="p-2 rounded-md w-full text-lg font-semibold text-white font-mono bg-[rgb(30,30,30)] hover:bg-[rgb(20,20,20)] hover:text-[rgb(225,225,225)] duration-300" onclick={props.onClick}>
+                <button class="p-2 rounded-md w-full font-semibold text-white font-mono bg-[rgb(30,30,30)] hover:bg-[rgb(20,20,20)] hover:text-[rgb(225,225,225)] duration-300" onclick={props.onClick}>
                     {props.buttonText}
                 </button>
             </div>
