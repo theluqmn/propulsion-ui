081b2e3b3035bd889de7110eded1b63d4a365f5e
luq-mne <luqman.yazrie@outlook.com>
2025-01-01 11:19:51 +0800

    done for frames

---
+++ src/components/frames/Frame.jsx
@@ -0,0 +1,12 @@
+export const Frame = (props) => {
+    const baseColor = props.light ? 'bg-[rgb(225,225,225)]' : 'bg-[rgb(0,0,0)]';
+    const outlineColor = props.light ? 'bg-[rgb(205,205,205)]' : 'bg-[rgb(20,20,20)]';
+
+    return (
+        <div class={`p-2 rounded-md ${outlineColor}`}>
+            <div class={`p-2 rounded-md ${baseColor}`}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/frames/FrameSolid.jsx
@@ -0,0 +1,9 @@
+export const FrameSolid = (props) => {
+    const baseColor = props.light ? 'bg-[rgb(225,225,225)]' : 'bg-[rgb(10,10,10)]';
+
+    return  (
+        <div class={`p-4 rounded-md ${baseColor}`}>
+            {props.children}
+        </div>
+    )
+}
