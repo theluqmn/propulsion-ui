6ae161cee0d134dea9de51adfba3c79921d90a2d
luq-mne <luqman.yazrie@outlook.com>
2025-01-01 17:46:16 +0800

    new props for textSolid

--- src/components/texts/TextSolid.jsx
+++ src/components/texts/TextSolid.jsx
@@ -1,7 +1,13 @@
 const TextSolid = (props) => {
+    const textSize = props.textSize || "text-base";
+    const textColor = props.textColor || "text-white";
+    const textWeight = props.textWeight || "text-normal";
+    const textAlign = props.textAlign || "text-left";
+    const text = props.text || "Default Text"
+
     return (
         <div class="p-4 rounded-md bg-black">
-            <p class="text-white font-mono text-sm md:text-base hover:text-[rgb(225,225,225)] duration-300">
+            <p class={`${textColor} font-mono ${textSize} ${textWeight} ${textAlign} hover:brightness-90 duration-300`}>
                 {props.text}
             </p>
         </div>
